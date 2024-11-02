89f58161c10df8eda362459d716fd5558073fdc4
AmirShariat <amishari@yahoo.com>
2024-11-02 21:05:24 +0330

    supbase initialized

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,4 +1,20 @@
-function Bookings() {
-  return <div>Cabins</div>;
+import { useEffect } from 'react';
+import { getCabins } from '../services/apiCabins';
+
+function Cabins() {
+  useEffect(() => {
+    getCabins().then((data) => console.log(data));
+  }, []);
+  return (
+    <div className="flex items-center justify-between">
+      <h1 className="text-5xl font-semibold">All Cabins</h1>
+      <p>test</p>
+      <img
+        src="https://lcrwrkpnlcepptcaqrml.supabase.co/storage/v1/object/public/cabins/cabin-001.jpg?t=2024-11-02T16%3A36%3A00.189Z"
+        alt="cabin01"
+        srcset=""
+      />
+    </div>
+  );
 }
-export default Bookings;
+export default Cabins;
---
+++ src/services/supabase.js
@@ -0,0 +1,9 @@
+import { createClient } from '@supabase/supabase-js';
+
+const SUPABASE_KEY =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxjcndya3BubGNlcHB0Y2Fxcm1sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA0ODYzNDMsImV4cCI6MjA0NjA2MjM0M30.BDqgoieJ1epmI_ShuXIRfhOvKVG9zuUAdumGbstvCWM';
+
+const SUPABASE_URL = 'https://lcrwrkpnlcepptcaqrml.supabase.co';
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export default supabase;
