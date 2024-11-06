5f027e02432772de412b949545d03cbe1bd846f6
AmirShariat <amishari@yahoo.com>
2024-11-06 12:08:23 +0330

    partial

---
+++ src/features/cabins/CabinHeader.jsx
@@ -0,0 +1,15 @@
+function CabinHeader() {
+  return (
+    <div className="grid grid-cols-[9.6rem_0.8fr_2fr_1fr_1fr_3.2rem] border-b-2 border-gray-400 bg-gray-50 px-6 py-10 font-semibold capitalize tracking-wider text-gray-600">
+      <div></div>
+
+      <div>Cabin</div>
+      <div>Capacity</div>
+      <div>Price</div>
+      <div>Discount</div>
+      <div></div>
+    </div>
+  );
+}
+
+export default CabinHeader;
