65ca85107a66d72291c444ce177d0a7984a1a394
AmirShariat <amishari@yahoo.com>
2024-11-06 19:19:59 +0330

    some improvement in headerCabin

--- src/features/cabins/CabinHeader.jsx
+++ src/features/cabins/CabinHeader.jsx
@@ -1,6 +1,6 @@
 function CabinHeader() {
   return (
-    <div className="mb-14 flex items-center justify-around justify-items-start border-gray-300">
+    <div className="flex items-center justify-between border-gray-300 p-8">
       <h1 className="text-4xl">All Cabins</h1>
       <p>Sort/filter</p>
     </div>
