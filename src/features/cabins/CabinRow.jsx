4eb88deefcd6743d274aa42dc2f9ad815cb1d6bd
AmirShariat <amishari@yahoo.com>
2024-11-06 19:30:50 +0330

    supabase fixed

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,7 +1,7 @@
 function CabinRow({ cabin }) {
   const { name, maxCapacity, regularPrice, discount, image } = cabin;
   return (
-    <div className="grid grid-cols-[0.6fr_1.8fr_2.2fr_1fr_1fr_1fr] gap-10 rounded-lg border-2 border-b border-gray-200 px-10 py-6 last:border-b-0">
+    <div className="grid grid-cols-[0.6fr_1.8fr_2.2fr_1fr_1fr_1fr] justify-items-start gap-10 rounded-lg border-2 border-b border-gray-200 px-10 py-6 last:border-b-0">
       <img
         className="aspect-[3/2] w-28 scale-150 object-cover object-center"
         src={image}
