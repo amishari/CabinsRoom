b0b32308eb9a2688bbc28dab84e6a288622d93c6
AmirShariat <amishari@yahoo.com>
2024-11-06 18:58:05 +0330

    spinner cabinRow comp added.

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,5 +1,19 @@
-function CabinRow() {
-  return <div></div>;
+function CabinRow({ cabin }) {
+  const { name, maxCapacity, regularPrice, discount, image } = cabin;
+  return (
+    <div className="grid grid-cols-[0.6fr_1.8fr_2.2fr_1fr_1fr_1fr] gap-10 rounded-lg border-2 border-b border-gray-200 px-6 py-10 last:border-b-0">
+      <img
+        className="aspect-[3/2] w-28 scale-150 object-cover object-center"
+        src={image}
+        alt="not"
+        srcset=""
+      />
+      <div className="text-2xl font-semibold text-gray-600">{name}</div>
+      <div className="text-2xl font-semibold">{maxCapacity}</div>
+      <div className="text-2xl font-semibold">{regularPrice}</div>
+      <div className="text-2xl font-semibold text-gray-800">{discount}</div>
+    </div>
+  );
 }
 
 export default CabinRow;
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,12 +1,26 @@
+import { useQuery } from '@tanstack/react-query';
+import CabinRow from '../features/cabins/CabinRow';
+import { getCabins } from '../services/apiCabins';
+import Spinner from '../ui/Spinner';
+
 function Cabins() {
+  const {
+    isLoading,
+    data: cabins,
+    error,
+  } = useQuery({
+    queryKey: ['cabins'],
+    queryFn: getCabins,
+  });
+  if (isLoading) return <Spinner />;
   return (
     <>
-      <div className="border-gray-150 overflow-hidden rounded-lg border-2 bg-gray-50 px-9 py-5 text-2xl">
-        <div className="flex items-center justify-around justify-items-start border-b-2 border-gray-300">
+      <div className="border-gray-150 overflow-hidden rounded-lg border-2 border-gray-200 bg-gray-50 px-9 py-5 text-2xl">
+        <div className="mb-14 flex items-center justify-around justify-items-start border-gray-300">
           <h1 className="text-4xl">All Cabins</h1>
           <p>Sort/filter</p>
         </div>{' '}
-        <div className="grid grid-cols-[0.6fr_1.8fr_2.2fr_1fr_1fr_1fr]">
+        <div className="grid grid-cols-[0.6fr_1.8fr_2.2fr_1fr_1fr_1fr] gap-10 rounded-lg border-2 border-gray-200 p-6">
           <div></div>
           <div>Cabin</div>
           <div>Capacity</div>
@@ -14,6 +28,9 @@ function Cabins() {
           <div>Discount</div>
           <div></div>
         </div>
+        {cabins.map((cabin) => (
+          <CabinRow cabin={cabin} key={cabin.id} />
+        ))}
       </div>
     </>
   );
