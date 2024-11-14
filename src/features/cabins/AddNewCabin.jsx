54dba7a3c0c870f14acea7ff0bda7d7991dc6b72
AmirShariat <amishari@yahoo.com>
2024-11-14 16:36:29 +0330

    working on Modal as compound component pattern

--- src/features/cabins/AddNewCabin.jsx
+++ src/features/cabins/AddNewCabin.jsx
@@ -1,22 +1,34 @@
 import { useState } from 'react';
-import Modal from '../../ui/Modal';
+import Modal from '../../ui/Modal-v1';
 import CreateCabinForm from './CreateCabinForm';
 
 export default function AddNewCabin() {
-  const [isOpenModal, setIsOpenModal] = useState(false);
   return (
-    <div>
-      <button
-        onClick={() => setIsOpenModal((show) => !show)}
-        className="my-4 w-2/12 rounded-md border-2 border-blue-900 bg-blue-500 px-4 py-4 text-xl font-semibold text-white hover:bg-blue-400"
-      >
-        Add new Cabin
-      </button>
-      {isOpenModal && (
-        <Modal onClose={() => setIsOpenModal(false)}>
-          <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
-        </Modal>
-      )}
-    </div>
+    <Modal>
+      <Modal.Open opens="cabin-form">
+        <button>Add New Cabin</button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
+    </Modal>
   );
 }
+// export default function AddNewCabin() {
+//   const [isOpenModal, setIsOpenModal] = useState(false);
+//   return (
+//     <div>
+//       <button
+//         onClick={() => setIsOpenModal((show) => !show)}
+//         className="my-4 w-2/12 rounded-md border-2 border-blue-900 bg-blue-500 px-4 py-4 text-xl font-semibold text-white hover:bg-blue-400"
+//       >
+//         Add new Cabin
+//       </button>
+//       {isOpenModal && (
+//         <Modal onClose={() => setIsOpenModal(false)}>
+//           <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
+//         </Modal>
+//       )}
+//     </div>
+//   );
+// }
--- src/ui/Modal-v1.jsx
+++ src/ui/Modal-v1.jsx
@@ -1,21 +1,7 @@
-import { createContext, useContext, useState } from 'react';
 import { createPortal } from 'react-dom';
 import { HiXMark } from 'react-icons/hi2';
-import { useSubmit } from 'react-router-dom';
 
-const ModalContext = createContext();
-export default function Modal({children}){
-    const [openName,setOpenName]=useState('');
-    const close= ()=>setOpenName('')
-    const open = ()=>setOpenName('')
-    function Open({children,opens}){
-        const{open}=useContext(ModalContext)
-        return children
-    }
-}
-Modal.Open=Open
-
-export default function Window({ children, onClose }) {
+export default function Modal({ children, onClose }) {
   return createPortal(
     <div className="z-1000 fixed left-0 top-0 h-screen w-full bg-white bg-opacity-10 backdrop-blur-sm transition duration-500">
       <div className="fixed left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%] rounded-lg bg-white px-16 py-12 shadow-lg transition duration-500">
