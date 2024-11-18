9680ee8498263f57a6a681b641de0bf87bb0501e
AmirShariat <amishari@yahoo.com>
2024-11-18 19:59:06 +0330

    make filter reusable

--- src/features/cabins/CabinTableOperation.jsx
+++ src/features/cabins/CabinTableOperation.jsx
@@ -2,7 +2,14 @@ import Filter from '../../ui/Filter';
 function CabinTableOperation() {
   return (
     <div className="flex items-center gap-2">
-      <Filter />
+      <Filter
+        filterField="discount"
+        options={[
+          { value: 'all', label: 'All' },
+          { value: 'no-discount', label: 'No Discount' },
+          { value: 'with-discount', label: 'with Discount' },
+        ]}
+      />
     </div>
   );
 }
--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -1,24 +1,24 @@
 import { useSearchParams } from 'react-router-dom';
 
-function Filter() {
+function Filter({ filterField, options }) {
   const [searchParams, setSearchParams] = useSearchParams();
   const but =
     'active:bg-blue-300 hover:bg-blue-600 hover:text-brand-50 active:text-brand-50 rounded-xl border-none bg-white px-[0.8rem] py-[0.44rem] text-[1.4rem] font-semibold transition-all duration-400 disabled:opacity-50';
   function handleClick(value) {
-    searchParams.set('discount', value);
+    searchParams.set(filterField, value);
     setSearchParams(searchParams);
   }
   return (
     <div className="flex gap-1.5 rounded-xl border-2 bg-white p-1.5 shadow-lg">
-      <button onClick={() => handleClick('all')} className={but}>
-        All
-      </button>
-      <button onClick={() => handleClick('no-discount')} className={but}>
-        No Discount
-      </button>
-      <button onClick={() => handleClick('with-discount')} className={but}>
-        with Discount
-      </button>
+      {options.map((option) => (
+        <button
+          onClick={() => handleClick(option.value)}
+          className={but}
+          key={option.value}
+        >
+          {option.label}
+        </button>
+      ))}
     </div>
   );
 }
