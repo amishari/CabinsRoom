e4d02c507b9cc71cbf6a26dd5b3fb2b62585819b
AmirShariat <amishari@yahoo.com>
2024-11-24 22:23:01 +0330

    filter added to bboking

--- src/features/bookings/BookingHeader.jsx
+++ src/features/bookings/BookingHeader.jsx
@@ -1,8 +1,10 @@
+import BookingTableOperations from './BookingTableOperations';
+
 function BookingHeader() {
   return (
     <div className="flex items-center justify-between border-gray-300 p-8">
       <h1 className="text-4xl">All Booking</h1>
-      <p>Test</p>
+      <BookingTableOperations />
     </div>
   );
 }
--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,22 @@
 import { useQuery } from '@tanstack/react-query';
 import { getBookings } from '../../services/apiBooking';
+import { useSearchParams } from 'react-router-dom';
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+  //filter
+  const filterValue = searchParams.get('status');
+  const filter =
+    !filterValue || filterValue === 'all'
+      ? null
+      : { field: 'status', value: filterValue };
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ['booking'],
-    queryFn: () => getBookings(),
+    queryKey: ['booking', filter],
+    queryFn: () => getBookings({ filter }),
     retry: false,
   });
   return { isLoading, bookings, error };
