4c267f648f1aa3e2bec370a2325bd03f2b2b5344
AmirShariat <amishari@yahoo.com>
2024-12-01 18:44:04 +0330

    h1 in signuo fixed

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -3,7 +3,7 @@ import SignupForm from '../features/authentication/SignupForm';
 function Users() {
   return (
     <div>
-      <h1>Signup here</h1>
+      <h1 className="text-6xl font-semibold">Signup here</h1>
       <SignupForm />
     </div>
   );
